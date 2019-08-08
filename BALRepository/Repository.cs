696a2155810d09bebdffeb119d0311306e1ecbb5
SParvezz <53895247+SParvezz@users.noreply.github.com>
2019-08-08 12:34:04 +0000

    commented error code

--- BALRepository/Repository.cs
+++ BALRepository/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BALRepository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,39 +7,39 @@ using System.Threading.Tasks;
 
 namespace BALRepository
 {
-    public class Repository<T> : IRepository<T> where T : IEntity
-    {
-        private UnitOfWork _unitOfWork;
-        public Repository(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = (UnitOfWork)unitOfWork;
-        }
+    //public class Repository<T> : IRepository<T> where T : IEntity
+    //{
+    //    private UnitOfWork _unitOfWork;
+    //    public Repository(IUnitOfWork unitOfWork)
+    //    {
+    //        _unitOfWork = (UnitOfWork)unitOfWork;
+    //    }
 
-        protected ISession Session { get { return _unitOfWork.Session; } }
+    //    protected ISession Session { get { return _unitOfWork.Session; } }
 
-        public IQueryable<T> GetAll()
-        {
-            return Session.Query<T>();
-        }
+    //    public IQueryable<T> GetAll()
+    //    {
+    //        return Session.Query<T>();
+    //    }
 
-        public T GetById(int id)
-        {
-            return Session.Get<T>(id);
-        }
+    //    public T GetById(int id)
+    //    {
+    //        return Session.Get<T>(id);
+    //    }
 
-        public void Create(T entity)
-        {
-            Session.Save(entity);
-        }
+    //    public void Create(T entity)
+    //    {
+    //        Session.Save(entity);
+    //    }
 
-        public void Update(T entity)
-        {
-            Session.Update(entity);
-        }
+    //    public void Update(T entity)
+    //    {
+    //        Session.Update(entity);
+    //    }
 
-        public void Delete(int id)
-        {
-            Session.Delete(Session.Load<T>(id));
-        }
-    }
+    //    public void Delete(int id)
+    //    {
+    //        Session.Delete(Session.Load<T>(id));
+    //    }
+    //}
 }
