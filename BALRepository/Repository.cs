789d7acc2d6524ee69e29e9cb486bb65d997bfb7
Amith2019 <53898709+Amith2019@users.noreply.github.com>
2019-08-08 11:39:24 +0000

    Unit of work

---
+++ BALRepository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BALRepository
+{
+    public class Repository<T> : IRepository<T> where T : IEntity
+    {
+        private UnitOfWork _unitOfWork;
+        public Repository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = (UnitOfWork)unitOfWork;
+        }
+
+        protected ISession Session { get { return _unitOfWork.Session; } }
+
+        public IQueryable<T> GetAll()
+        {
+            return Session.Query<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return Session.Get<T>(id);
+        }
+
+        public void Create(T entity)
+        {
+            Session.Save(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Session.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            Session.Delete(Session.Load<T>(id));
+        }
+    }
+}
---
+++ BALRepository/UnitOfWork .cs
@@ -0,0 +1,40 @@
+﻿using BALRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BALRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+
+        static UnitOfWork()
+        {
+
+        }
+
+        public UnitOfWork()
+        {
+
+        }
+
+        public void BeginTransaction()
+        {
+
+        }
+
+        public void Commit()
+        {
+        }
+
+
+
+        public void Rollback()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
